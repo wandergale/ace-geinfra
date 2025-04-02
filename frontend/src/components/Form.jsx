cb99fed15baff7633a51c1aaf1d162055370df3a
Wanderson notebook <wandersonlife7@gmail.com>
2025-04-02 20:29:36 -0300

    primeiro component

---
+++ frontend/src/components/Form.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Form.css";
+
+const Form = () => {
+  return (
+    <div className="form-container">
+      <p>Ola mundo</p>
+    </div>
+  );
+};
+
+export default Form;
